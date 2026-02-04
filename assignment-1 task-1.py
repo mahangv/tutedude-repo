05884a685e5ed87f718f58bd8ae1358a03464951
mahangv <mahantesh93gv@gmail.com>
2026-02-04 23:29:05 +0530

    assignment 1 files

---
+++ assignment-1 task-1.py
@@ -0,0 +1,22 @@
+number1 = int(input("enter the first number "))
+number2 = int(input("enter the second number "))
+
+add = number1 + number2
+subt = number1-number2
+mult = number1 * number2
+div = number1 / number2
+
+print(f"The sum of provided  numbers is {add} ")
+print(f"The difference of provided  numbers is {subt} ")
+print(f"The product  of provided  numbers is {mult} ")
+print(f"The division of provided  numbers is {div} ")
+
+#print(f"The sum of provided  numbers are  {number1 + number2} ")
+#print(f"The sum of provided  numbers are  {number1 - number2} ")
+#print(f"The sum of provided  numbers are  {number1 * number2} ")
+#print(f"The sum of provided  numbers are  {number1 / number2} ")
+
+
+
+
+
---
+++ assignment-1 task-2.py
@@ -0,0 +1,7 @@
+firstName = str(input("please enter your first name "))
+lastname = str(input("please enter ypur last name "))
+
+print(f" Hello {firstName + lastname} ! welcome to python program ")
+
+
+
