2a2bc0f483d7d2ca5209a6f5d5ae605bc5072b37
mahangv <mahantesh93gv@gmail.com>
2026-02-05 20:50:22 +0530

    Assignment 2 tasks done

---
+++ assignment-2 task-1.py
@@ -0,0 +1,10 @@
+oddoreve = int(input("enter a number to check odd or even "))
+
+if (oddoreve % 2 ==0) :
+    print (f"The num {oddoreve} is even")
+
+else :
+    print (f"the number {oddoreve} is odd")
+
+
+
